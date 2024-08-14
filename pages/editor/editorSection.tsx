61c5f3e5643376f8fe7d58227a88b59cb412f5e1
Kai Sato <ksato@rpmglobal.com>
2024-08-14 23:11:36 +1000

    First UI changes

--- pages/editor/editorSection.tsx
+++ pages/editor/editorSection.tsx
@@ -1,97 +1,137 @@
-import { Avatar, Button, Container, Grid, IconButton, List, ListItem, ListItemAvatar, ListItemText, Stack, TextField, Typography } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  Container,
+  Grid,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Paper,
+  TextField,
+  Typography
+} from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
-import AddIcon from '@mui/icons-material/Add';
-import { useState } from "react";
+import { useState } from 'react';
 import Kai from '../../template/Kai.json';
-const ContentSpacer = () => <Container sx={{height:12}}></Container>
+import TypedText from '../../common/text';
+const ContentSpacer = () => <Container sx={{ height: 12 }}></Container>;
 
-export default function Editor()  { 
+export default function Editor() {
   const [state, setState] = useState(Kai);
-  
 
-  const appendAttribute = (attribute : Attributes) => {
-    setState({...state,header:{...state.header,
-      attributes: [...state.header.attributes, attribute],
-    }})
-  }
- 
-   
-  
-  return <Stack>
-            <Typography align="center" variant="h3" component="h2">
-            Editor
-            </Typography>
-              <Typography variant="h4" component="h2">
-              Header
-            </Typography>
-            <TextField  InputLabelProps={{ shrink: true }} placeholder='e.g. John Smith' label="Name" variant="outlined" />
-            <ContentSpacer/>
-            <TextField  InputLabelProps={{ shrink: true }} placeholder='' multiline={true} minRows={2} rows={2} label="About Me" variant="outlined" />
-            <Grid container spacing={2} direction='row'>
-              <Grid item xs={8}>
-                <Typography variant="h5" component="h2">
-                  Attributes (left side list)
-                </Typography>
-              </Grid>
-              <Grid item xs={8}>
-                <Button variant="outlined" startIcon={<AddIcon />} onClick={() => appendAttribute({icon:'',text:''})}>
-                  Add
-                </Button>
-              </Grid>
-            <Grid item xs={8}>
-              <List dense={true}>
-             {state.header.attributes.map((item) => { return  <ListItem
-                  secondaryAction={
-                    <IconButton edge="end" aria-label="delete">
-                      <EditIcon  style={{ color:'#1d62fb'}} />
-                    </IconButton>
-                  }
-                >
-                  <ListItemAvatar>
-                    <Avatar>
-                    </Avatar>
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary={item.text}
-                    secondary={ 'Secondary text'}
-                  />
-                </ListItem>
-             } )}
-                </List>
-              </Grid>
-            </Grid>
-            
-            <List dense={true}>
+  const appendAttribute = (attribute: Attributes) => {
+    setState({
+      ...state,
+      header: {
+        ...state.header,
+        attributes: [...state.header.attributes, attribute]
+      }
+    });
+  };
+
+  return (
+    <Paper
+      sx={{
+        margin: 4,
+        padding: 3,
+        borderRadius: 5,
+        boxShadow:
+          ' 0 5px 10px rgba(154,160,185,.05), 0 15px 40px rgba(166,173,201,.2)'
+      }}
+    >
+      <TypedText textVariant="H1Headline">React Resume</TypedText>
+      <Typography variant="h4" component="h2">
+        Header
+      </Typography>
+      <TextField
+        InputLabelProps={{ shrink: true }}
+        placeholder="e.g. John Smith"
+        label="Name"
+        variant="outlined"
+      />
+      <ContentSpacer />
+      <TextField
+        InputLabelProps={{ shrink: true }}
+        placeholder=""
+        multiline={true}
+        minRows={2}
+        rows={2}
+        label="About Me"
+        variant="outlined"
+      />
+      <Grid container spacing={2} direction="row">
+        <Grid item xs={8}>
+          <Typography variant="h5" component="h2">
+            Attributes (left side list)
+          </Typography>
+        </Grid>
+        <Grid item xs={8}>
+          <Button
+            variant="outlined"
+            onClick={() => appendAttribute({ icon: '', text: '' })}
+          >
+            Add
+          </Button>
+        </Grid>
+        <Grid item xs={8}>
+          <List dense={true}>
+            {state.header.attributes.map((item) => {
+              return (
                 <ListItem
                   secondaryAction={
                     <IconButton edge="end" aria-label="delete">
-                      <EditIcon  style={{ color:'#1d62fb'}} />
+                      <EditIcon style={{ color: '#1d62fb' }} />
                     </IconButton>
                   }
                 >
                   <ListItemAvatar>
-                    <Avatar>
-                    </Avatar>
+                    <Avatar></Avatar>
                   </ListItemAvatar>
                   <ListItemText
-                    primary="Single-line item"
-                    secondary={ 'Secondary text'}
+                    primary={item.text}
+                    secondary={'Secondary text'}
                   />
                 </ListItem>
-            </List>
-            <Typography variant="h5" component="h2">
-              Attributes
-            </Typography>
-            <Typography  variant="h4" component="h2">
-              Body
-            </Typography>
-            <Typography variant="h5" component="h2">
-              Left Side
-            </Typography>
-            <Typography variant="h5" component="h2">
-              Right Side
-            </Typography>
-            <Typography variant="h5" component="h2">
-              Bottom
-            </Typography>
-          </Stack> }
+              );
+            })}
+          </List>
+        </Grid>
+      </Grid>
+
+      <List dense={true}>
+        <ListItem
+          secondaryAction={
+            <IconButton edge="end" aria-label="delete">
+              <EditIcon style={{ color: '#1d62fb' }} />
+            </IconButton>
+          }
+        >
+          <ListItemAvatar>
+            <Avatar></Avatar>
+          </ListItemAvatar>
+          <ListItemText
+            primary="Single-line item"
+            secondary={'Secondary text'}
+          />
+        </ListItem>
+      </List>
+      <Typography variant="h5" component="h2">
+        Attributes
+      </Typography>
+      <Typography variant="h4" component="h2">
+        Body
+      </Typography>
+      <Typography variant="h5" component="h2">
+        Left Side
+      </Typography>
+      <Typography variant="h5" component="h2">
+        Right Side
+      </Typography>
+      <Typography variant="h5" component="h2">
+        Bottom
+      </Typography>
+    </Paper>
+  );
+}
--- pages/editor/layoutOutline.tsx
+++ pages/editor/layoutOutline.tsx
@@ -1,23 +1,15 @@
-
-import { Grid } from '@mui/material';
+import { Box } from '@mui/material';
 import Editor from './editorSection';
 import Preview from './previewSection';
-import TitleBar from '../../common/titleBar';
+import Scaffold from '../../common/scaffold';
 
-export default function editorOutline
-() {
+export default function editorOutline() {
   return (
-    <Grid container spacing={2}>
-    <Grid item xs={12}>
-      <TitleBar />
-    </Grid>
-    <Grid item xs={6}>
-      <Editor />
-    </Grid>
-    <Grid item xs={6}>
-       <Preview />
-    </Grid>
-    </Grid>
-    
+    <Scaffold>
+      <Box sx={{ display: 'flex' }}>
+        <Editor />
+        <Preview />
+      </Box>
+    </Scaffold>
   );
 }
